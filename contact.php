68b6c89cf22be9ddf8c16aa232c5235a5c030754
michelle <michelle@frissr.com>
2017-07-28 23:01:33 +0200

    fix button now working

--- contact.php
+++ contact.php
@@ -1,27 +1,46 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$emailFrom = "pistersmichelle@gmail.com";
+$emailTo = "pistersmichelle@gmail.com";
+$subject = "Contact Form Submission";
 
-// Create the email and send the message
-$to = 'pistersmichelle@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-echo "Werkt dit????????????"
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $phone = $message = "";
+function validate_data($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	if(empty($_POST["name"]){
+		$nameErr = "Name is required";
+	} 
+	$name = validate_data($_POST["name"]);
+	if(empty($_POST["email"]){
+		$emailErr = "Email is required";
+	} 
+	$email = validate_data($_POST["email"]);
+	$message = validate_data($_POST["message"]);
+	$phone = validate_data($_POST["phone"]);
+}
+
+$body .= "Name: ";
+$body .= $name;
+$body .= "\n";
+ 
+$body .= "Email: ";
+$body .= $email;
+$body .= "\n";
+ 
+$body .= "Message: ";
+$body .= $message;
+$body .= "\n";
+
+
+if($succes){
+	echo "hello world";
+} else {
+	echo "nope";
+}
 ?>
